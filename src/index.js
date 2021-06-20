e80b2f97d4ac74d6ad16a6472b0c34aad041f7bd
Edgar Ulises Ciprés Medina <48774929+UlisesCm@users.noreply.github.com>
2021-06-20 18:23:37 -0500

    se agrego la tercer parte del hook y se agrego el custom hook de useFetch

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 /* import { HookApp } from './HookApp' */
 /* import { CounterApp } from './components/01-useState/CounterApp' */
 /* import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook' */
-import { SimpleForm } from './components/02-useEffect/SimpleForm'
+/* import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook' */
+import { MultipleCustomHook } from './components/03-examples/MultipleCustomHook'
 
 ReactDOM.render(
-    <SimpleForm />,
+    <MultipleCustomHook />,
   document.getElementById('root')
 );
 
