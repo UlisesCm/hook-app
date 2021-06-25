fbd4fab4c951dda6cfd9e1aa453f6a800f4c0696
Edgar Ulises Ciprés Medina <48774929+UlisesCm@users.noreply.github.com>
2021-06-25 00:38:37 -0500

    se agrego funcionalidad de LocalStorage

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,16 +1,18 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export const useForm = ( initialState = {}) => {
+export const useForm = (initialState = {}) => {
+  const [values, setValues] = useState(initialState);
 
-    const [values, setValues] = useState(initialState)
+  const reset = () => {
+    setValues(initialState);
+  };
 
-    const handleInputChange = ({target}) =>{
-        
-        setValues({
-            ...values,
-            [target.name]:target.value
-        })
-    }
+  const handleInputChange = ({ target }) => {
+    setValues({
+      ...values,
+      [target.name]: target.value,
+    });
+  };
 
-    return [values, handleInputChange];
-}
+  return [values, handleInputChange, reset];
+};
